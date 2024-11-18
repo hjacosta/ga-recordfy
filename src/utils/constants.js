c633e5727901760607897f60f26673aadd481ff1
acostahenry7 <73194577+acostahenry7@users.noreply.github.com>
2024-11-18 09:41:10 -0400

    fix-point-to-backend

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-  serverURL: "http://localhost:3001/api",
-  // serverURL: "http://op.grupoavant.com.do:19605/api",
+  // serverURL: "http://localhost:3001/api",
+  serverURL: "http://op.grupoavant.com.do:19605/api",
   //serverURL: "http://172.16.0.212:3001/api",
   // serverURL: "http://10.1.102.31:3001/api",
   // serverURL: "https://h6v791zx-3001.use2.devtunnels.ms/api",
